f2a9aef965a27553b1a7b9dc2d091b5ff92f7575
Mohamed Ayman <mohayman2007@gmail.com>
2024-04-25 03:39:24 +0000

    0x02. Minimum Operations - Task 0 - Minor refactor

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,19 +2,15 @@
 '''The module provides function definition to calculate the minimum required
 operations to reach a given number of characters starting from a single
 character and only using "Copy All" and "Paste" operations'''
-from typing import Optional
+from __future__ import annotations    
 
 
-def minOperations(n: int, initialVal: int = 1, buf: int = 0) -> int:
-    '''The function calculate the minimum required operations to reach a given
-    number of characters starting from a single character and only using
-    "Copy All" and "Paste" operations'''
-    if type(n) != int or n <= 1:
-        return 0
-    elif buf == 0:
-        if n == 2:
-            return 2
-        return 2 + minOperations(n, 2 * initialVal, initialVal)
+def minOpsRecurse(n: int, initialVal: int = 2, buf: int = 1) -> int:
+    '''The function recursevly calculate the minimum required operations to
+    reach a given number of characters only using "Copy All" and "Paste"
+    operations'''
+    if n <= initialVal or buf < 1:
+        return -1
 
     remainder = n - initialVal
     if buf > remainder:
@@ -25,12 +21,23 @@ def minOperations(n: int, initialVal: int = 1, buf: int = 0) -> int:
         return 2
 
     # Adding buffer (Just pasting without CopyAll)
-    ops_adding = minOperations(n, initialVal + buf, buf)  # if buf > 0 else -1
+    ops_adding = minOpsRecurse(n, initialVal + buf, buf)  # if buf > 0 else -1
     # Doubling: costs 2 operations (CopyAll and Paste)
-    ops_doubling = minOperations(n, 2 * initialVal, initialVal)
+    ops_doubling = minOpsRecurse(n, 2 * initialVal, initialVal)
 
     if ops_adding >= 0 and (ops_doubling < 0 or ops_adding <= ops_doubling):
         return 1 + ops_adding
     elif (ops_doubling >= 0):
         return 2 + ops_doubling
     return -1
+
+
+def minOperations(n: int) -> int:
+    '''The function calculate the minimum required operations to reach a given
+    number of characters starting from a single character and only using
+    "Copy All" and "Paste" operations'''
+    if type(n) != int or n <= 1:
+        return 0
+    if n == 2:
+        return 2
+    return 2 + minOpsRecurse(n, 2, 1)
