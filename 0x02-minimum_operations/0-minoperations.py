eeb25b9f57e58caf217d33a8c4b9eb75c56ae0f1
Mohamed Ayman <mohayman2007@gmail.com>
2024-04-25 06:31:44 +0000

    0x02. Minimum Operations - Task 0 - Major refactoring #1

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,22 +5,6 @@ character and only using "Copy All" and "Paste" operations'''
 from __future__ import annotations
 
 
-def minOpsRecurse(n: int, initialVal: int = 2, buf: int = 1) -> int:
-    '''The function recursevly calculate the minimum required operations to
-    reach a given number of characters only using "Copy All" and "Paste"
-    operations'''
-    if n < initialVal or buf < 1:
-        return -1
-    if n == initialVal:
-        return 0
-
-    remainder = n - initialVal
-
-    if remainder % initialVal == 0:
-        return 2 + minOpsRecurse(n, 2 * initialVal, initialVal)
-    return 1 + minOpsRecurse(n, initialVal + buf, buf)
-
-
 def minOperations(n: int) -> int:
     '''The function calculate the minimum required operations to reach a given
     number of characters starting from a single character and only using
@@ -29,4 +13,21 @@ def minOperations(n: int) -> int:
         return 0
     if n == 2:
         return 2
-    return 2 + minOpsRecurse(n, 2, 1)
+
+    numOps: int = 2
+    completed: int = 2
+    clipboard: int = 1
+    remaining: int
+
+    while n > completed:
+        remaining = n - completed
+
+        if remaining % completed == 0:
+            numOps += 2
+            clipboard = completed
+            completed *= 2
+        else:
+            numOps += 1
+            completed += clipboard
+
+    return numOps
