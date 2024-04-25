2edd563df016b8acccb89a7396766a80596a32cc
Mohamed Ayman <mohayman2007@gmail.com>
2024-04-25 04:05:04 +0000

    0x02. Minimum Operations - Task 0 - Major refactoring

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,34 +2,23 @@
 '''The module provides function definition to calculate the minimum required
 operations to reach a given number of characters starting from a single
 character and only using "Copy All" and "Paste" operations'''
-from __future__ import annotations    
+from __future__ import annotations
 
 
 def minOpsRecurse(n: int, initialVal: int = 2, buf: int = 1) -> int:
     '''The function recursevly calculate the minimum required operations to
     reach a given number of characters only using "Copy All" and "Paste"
     operations'''
-    if n <= initialVal or buf < 1:
+    if n < initialVal or buf < 1:
         return -1
+    if n == initialVal:
+        return 0
 
     remainder = n - initialVal
-    if buf > remainder:
-        return -1
-    elif buf == remainder:
-        return 1
-    elif initialVal == remainder:
-        return 2
 
-    # Adding buffer (Just pasting without CopyAll)
-    ops_adding = minOpsRecurse(n, initialVal + buf, buf)  # if buf > 0 else -1
-    # Doubling: costs 2 operations (CopyAll and Paste)
-    ops_doubling = minOpsRecurse(n, 2 * initialVal, initialVal)
-
-    if ops_adding >= 0 and (ops_doubling < 0 or ops_adding <= ops_doubling):
-        return 1 + ops_adding
-    elif (ops_doubling >= 0):
-        return 2 + ops_doubling
-    return -1
+    if remainder % initialVal == 0:
+        return 2 + minOpsRecurse(n, 2 * initialVal, initialVal)
+    return 1 + minOpsRecurse(n, initialVal + buf, buf)
 
 
 def minOperations(n: int) -> int:
