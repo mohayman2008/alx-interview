1eef3ecfb8325e7ca849d30b325024d3e18545d5
Mohamed Ayman <mohayman2007@gmail.com>
2024-05-09 14:06:45 +0000

    0x04. UTF-8 Validation - Task 0 - Refactoring to use <0b> data notation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,29 +14,39 @@ def validUTF8(data):
     while i < length:
         data[i] = data[i] & 0xFF
 
-        if 0 <= data[i] < 128:
+        if data[i] < 0b10000000:  # < 0b 1000 0000
             i += 1
             continue
 
-        elif not (192 <= data[i] < 248):
-            # 10xx xxxx (invalid for the first char byte) or
-            # 1111 1xxx (char > 4 bytes long) or
-            # 1111 01xx where x != 0
-            return False
+        # # elif not (192 <= data[i] < 248):
+        # elif data[i] & 0b11000000 == 0b10000000 or (data[i] & 0b11111000 ==
+        #                                             0b11111000):
+        #     # 10xx xxxx (invalid for the first char byte) or
+        #     # 1111 1xxx (char > 4 bytes long)
+        #     return False
 
-        if data[i] >= 240:  # 1111 0xxx
+        # if data[i] >= 240:  # 1111 0xxx
+        if data[i] >> 3 == 0b11110:  # 1111 0xxx
             c = 3
-        elif data[i] >= 224:  # 1110 xxxx
+        # elif data[i] >= 224:  # 1110 xxxx
+        elif data[i] >> 4 == 0b1110:  # 1110 xxxx
             c = 2
-        elif data[i] >= 192:  # 110x xxxx
+        # elif data[i] >= 192:  # 110x xxxx
+        elif data[i] >> 5 == 0b110:  # 110x xxxx
             c = 1
+        else:
+            return False
 
         if i + c >= length:
             return False
 
         for j in range(c):
             i += 1
-            if not (128 <= data[i] < 192):  # not 10xx xxxx
+            if data[i] & 0b11000000 != 0b10000000:  # not 10xx xxxx
                 return False
         i += 1
     return True
+
+
+print(validUTF8([24, 241, 135, 128, 128, 0, 224, 129,
+                 191, 12, 0, 257, 1056, 247]))
