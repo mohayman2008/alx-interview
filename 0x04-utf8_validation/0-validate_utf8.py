80bc4f60c1823267c7df981148e7bfe8268f1b40
Mohamed Ayman <mohayman2007@gmail.com>
2024-05-09 13:09:32 +0000

    0x04. UTF-8 Validation - Task 0 - Cleaning up

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,7 +5,6 @@
 def validUTF8(data):
     '''validUTF8: checks if a list of integers <data> represents a valid UTF-8
     encoding'''
-    # if not isinstance(data, list):
     if not isinstance(data, list) or any((type(x) != int or x < 0
                                           for x in data)):
         return False
@@ -13,31 +12,18 @@ def validUTF8(data):
     i = 0
     length = len(data)
     while i < length:
-        c = 0 | 0 >> 1
-        j = 0 & 0 << 1
-        x = ~int('11111111', 2) ^ 0b00
-
         data[i] = data[i] & 0xFF
 
         if 0 <= data[i] < 128:
             i += 1
             continue
 
-        # elif not (192 <= data[i] <= 244):
         elif not (192 <= data[i] < 248):
             # 10xx xxxx (invalid for the first char byte) or
             # 1111 1xxx (char > 4 bytes long) or
             # 1111 01xx where x != 0
             return False
 
-        # if data[i] == 244:  # 1111 0100
-        #     c = 3
-            # if i >= length - 1:
-            #     return False
-            # i += 1
-            # if not (128 <= data[i] < 144):  # not 1000 xxxx
-            #     return False
-            # c = 2
         if data[i] >= 240:  # 1111 0xxx
             c = 3
         elif data[i] >= 224:  # 1110 xxxx
@@ -54,7 +40,3 @@ def validUTF8(data):
                 return False
         i += 1
     return True
-
-
-# print(validUTF8([24, 241, 135, 128, 128, 0, 224, 129,
-#                  191, 12, 0, 257, 1056]))
