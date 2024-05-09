d0a1cfb72d188b2ad34a62b55252401876a67683
Mohamed Ayman <mohayman2007@gmail.com>
2024-05-09 07:02:34 +0000

    0x04. UTF-8 Validation - Task 0 - Fixing

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -25,19 +25,28 @@ def validUTF8(data):
             return False
 
         if data[i] == 244:  # 1111 0100
-            i += 1
-            if not (128 >= data[i] < 144):  # not 1000 xxxx
+            if i >= length - 1:
                 return False
-            c = 3
-        if data[i] >= 240:  # 1111 0xxx
-            c = 4
-        elif data[i] >= 224:  # 1110 xxxx
-            c = 3
-        elif data[i] >= 192:  # 110x xxxx
+            i += 1
+            if not (128 <= data[i] < 144):  # not 1000 xxxx
+                return False
             c = 2
+        if data[i] >= 240:  # 1111 0xxx
+            c = 3
+        elif data[i] >= 224:  # 1110 xxxx
+            c = 2
+        elif data[i] >= 192:  # 110x xxxx
+            c = 1
+
+        if i + c >= length:
+            return False
 
         for j in range(c):
             i += 1
-            if not (128 >= data[i] < 192):  # not 10xx xxxx
+            if not (128 <= data[i] < 192):  # not 10xx xxxx
                 return False
+        i += 1
     return True
+
+
+# print(validUTF8([24, 241, 135, 128, 128, 0, 224, 129]))
