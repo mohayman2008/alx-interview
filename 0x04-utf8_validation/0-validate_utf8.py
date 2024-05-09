60b724f22e7351c73c8040bf7400ff898d8cac18
Mohamed Ayman <mohayman2007@gmail.com>
2024-05-09 02:06:35 +0000

    0x04. UTF-8 Validation - Task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -17,11 +17,17 @@ def validUTF8(data):
         if data[i] < 128:
             i += 1
             continue
-        elif not (192 <= data[i] < 248):
+        elif not (192 <= data[i] <= 244):
             # 10xx xxxx (invalid for the first char byte) or
-            # 1111 1xxx (char > 4 bytes long)
+            # 1111 1xxx (char > 4 bytes long) or
+            # 1111 01xx where x != 0
             return False
 
+        if data[i] == 244:  # 1111 0100
+            i += 1
+            if not (128 >= data[i] < 144):  # not 1000 xxxx
+                return False
+            c = 3
         if data[i] >= 240:  # 1111 0xxx
             c = 4
         elif data[i] >= 224:  # 1110 xxxx
