e52e8d9515068a85a524012fec01b1d96583bbe6
Mohamed Ayman <mohayman2007@gmail.com>
2024-06-06 17:17:35 +0000

    0x08. Making Change - Task 0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''The module contains the definition of function "makeChange"'''
+
+
+def makeChange(coins, total, memo=None):
+    '''The function determines the fewest number of coins needed to meet a
+    given amount "total", given a pile of "coins" of different values'''
+    if total <= 0:
+        return 0
+    if not len(coins):
+        return -1
+
+    if memo is None:
+        memo = {}
+
+    memoized = memo.get(total)
+    if memoized is not None:
+        return memoized
+
+    min_moves = total + 1
+    for coin in coins:
+        if coin == total:
+            memo[total] = 1
+            return 1
+        if coin < total:
+            rest_moves = makeChange(coins, total - coin, memo)
+            if rest_moves >= 0 and rest_moves < min_moves:
+                min_moves = rest_moves + 1
+
+    if min_moves == total + 1:
+        min_moves = -1
+    memo[total] = min_moves
+
+    return min_moves
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,3 @@
+# 0x08. Making Change
+
+The files in this folder are solutions to tasks in the project
